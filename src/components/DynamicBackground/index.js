ea6291c4f7e72bd961cde27c9e4342060d1f9054
jamesagib <jagib07@gmail.com>
2022-07-18 09:24:58 -0700

    first commit

---
+++ src/components/DynamicBackground/index.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react'
+import { Text, Image, View } from 'react-native'
+ 
+class DynamicBackground extends Component {
+   render() {
+       const { color, image, type } = this.props
+ 
+       // const bgImage = { uri: image }
+ 
+       // if (image) {
+       //  if (image.url) {
+       //      image = image.url
+       //  } else {
+       //      image = image
+       //  }
+       // }
+ 
+       const bgImage = { uri: image }
+ 
+       if(type === 'image') {
+           return (
+               <Image
+                   source={bgImage}
+                   resizeMode='cover'
+                   style={{ width: this.props._width, height: this.props._height }}
+               />
+           )
+       } else {
+           return (
+               <View style={{ flex: 1, justifyContent: 'center', backgroundColor: color, width: '100%',height: '100%' }}>
+                   <Text>{color}</Text>
+               </View>
+           )
+       }
+   }
+}
+ 
+export default DynamicBackground
